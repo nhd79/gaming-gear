6b9d8fe2c49591637b32450355173853ee8898f3
nhd79 <50512194+nhd79@users.noreply.github.com>
2021-02-16 18:00:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   <title>Shop Homepage - Start Bootstrap Template</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="css/shop-homepage.css" rel="stylesheet">
