c1e41afe7ad737208ef31f928ecacacf692028f1
nhd79 <50512194+nhd79@users.noreply.github.com>
2021-02-16 19:03:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
   <title>Shop Homepage - Start Bootstrap Template</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
+  <link href="/css/shop-homepage.css" rel="stylesheet">
   
   <script src='https://kit.fontawesome.com/a076d05399.js'></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
