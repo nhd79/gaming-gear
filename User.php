f7094494eee4dc2f344f5c009e6121376592e290
nhd79 <50512194+nhd79@users.noreply.github.com>
2021-02-16 18:58:33 +0700

    Update User.php

--- User.php
+++ User.php
@@ -18,7 +18,7 @@
   <title>Shop Homepage - Start Bootstrap Template</title>
 
   <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="css/shop-homepage.css" rel="stylesheet">
